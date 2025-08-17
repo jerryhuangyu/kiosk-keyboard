047e9d8f6d9f374b26d40febf928da3072a939ea
Jerry Huang <jerryhuang@tac-dynamics.com>
2025-08-17 21:37:16 +0800

    feat: update VirtualKeyboard to support language switching and enhance key handling with new icons

--- packages/ui/lib/components/VirtualKeyboard.tsx
+++ packages/ui/lib/components/VirtualKeyboard.tsx
@@ -1,25 +1,36 @@
 import { useStorage, virtualKeyboard } from "@extension/shared";
 import { virtualKeyboardStorage } from "@extension/storage";
-import { useReducer } from "react";
+import { ArrowBigUpDashIcon, ArrowBigUpIcon, ChevronDownIcon, DeleteIcon, GlobeIcon } from "lucide-react";
+import { type ReactNode, useReducer } from "react";
 
-type KeyboardMode = "english" | "number";
+export enum Language {
+  English = "english",
+  Number = "number",
+}
+
+export enum KeyboardVariant {
+  Uppercase = "uppercase",
+  Lowercase = "lowercase",
+  Number = "number",
+  ShiftNumber = "shift-number",
+}
 
 type KeyboardState = {
-  mode: KeyboardMode;
-  permanentShift: boolean; // layout switch 按鈕切換的永久大小寫
+  language: Language;
+  permanentShift: boolean; // Shift 永久切換
   temporaryShift?: boolean; // Shift 暫時切換
 };
 
 type Action =
-  | { type: "SWITCH_MODE"; mode: KeyboardMode }
+  | { type: "SWITCH_LANGUAGE"; language: Language }
   | { type: "TOGGLE_TEMP_SHIFT" }
   | { type: "TOGGLE_PERMANENT_SHIFT" }
   | { type: "RESET_TEMP_SHIFT" };
 
 function keyboardReducer(state: KeyboardState, action: Action): KeyboardState {
   switch (action.type) {
-    case "SWITCH_MODE":
-      return { ...state, mode: action.mode, temporaryShift: undefined };
+    case "SWITCH_LANGUAGE":
+      return { ...state, language: action.language, temporaryShift: undefined };
     case "TOGGLE_TEMP_SHIFT":
       return { ...state, temporaryShift: !state.temporaryShift };
     case "TOGGLE_PERMANENT_SHIFT":
@@ -39,83 +50,25 @@ const isShiftActive = (state: KeyboardState) => {
   return state.temporaryShift;
 };
 
-type KeyboardKey = {
-  char: string;
+type Key = {
+  char: ReactNode;
   value: string;
   minWidth?: string;
   onClick?: () => void;
+  onDblClick?: () => void;
 };
 
 export const VirtualKeyboard = () => {
   const { isActive } = useStorage(virtualKeyboardStorage);
   const [keyboardState, dispatch] = useReducer(keyboardReducer, {
-    mode: "english",
+    language: Language.English,
     permanentShift: false,
   });
 
   if (!isActive) return;
 
-  // 永久切換大小寫
-  const handleSwitchLayout = () => dispatch({ type: "TOGGLE_PERMANENT_SHIFT" });
-
-  const keyboardLayouts: Record<string, KeyboardKey[][]> = {
-    uppercase: [
-      [
-        { char: "Q", value: "Q" },
-        { char: "W", value: "W" },
-        { char: "E", value: "E" },
-        { char: "R", value: "R" },
-        { char: "T", value: "T" },
-        { char: "Y", value: "Y" },
-        { char: "U", value: "U" },
-        { char: "I", value: "I" },
-        { char: "O", value: "O" },
-        { char: "P", value: "P" },
-        { char: "[", value: "[" },
-        { char: "]", value: "]" },
-        { char: "\\", value: "\\" },
-        { char: "Backspace", value: "", minWidth: "140px" },
-      ],
-      [
-        { char: "A", value: "A" },
-        { char: "S", value: "S" },
-        { char: "D", value: "D" },
-        { char: "F", value: "F" },
-        { char: "G", value: "G" },
-        { char: "H", value: "H" },
-        { char: "J", value: "J" },
-        { char: "K", value: "K" },
-        { char: "L", value: "L" },
-        { char: ";", value: ";" },
-        { char: "'", value: "'" },
-        { char: "Enter", value: "\n", minWidth: "160px" },
-      ],
-      [
-        { char: "Z", value: "Z" },
-        { char: "X", value: "X" },
-        { char: "C", value: "C" },
-        { char: "V", value: "V" },
-        { char: "B", value: "B" },
-        { char: "N", value: "N" },
-        { char: "M", value: "M" },
-        { char: ",", value: "," },
-        { char: ".", value: "." },
-        { char: "/", value: "/" },
-        {
-          char: "Shift",
-          value: "",
-          minWidth: "120px",
-          onClick: () => dispatch({ type: "TOGGLE_TEMP_SHIFT" }),
-        },
-      ],
-      [
-        { char: ".com", value: ".com" },
-        { char: "@", value: "@" },
-        { char: "Space", value: " ", minWidth: "80%" },
-        { char: "X", value: "", onClick: () => virtualKeyboard.clearFocus() },
-      ],
-    ],
-    lowercase: [
+  const keyboardLayouts: Record<KeyboardVariant, Key[][]> = {
+    [KeyboardVariant.Lowercase]: [
       [
         { char: "q", value: "q" },
         { char: "w", value: "w" },
@@ -130,7 +83,7 @@ export const VirtualKeyboard = () => {
         { char: "[", value: "[" },
         { char: "]", value: "]" },
         { char: "\\", value: "\\" },
-        { char: "Backspace", value: "", minWidth: "140px" },
+        { char: <DeleteIcon className="size-[18px] stroke-[1.5]" />, value: "{", minWidth: "140px" },
       ],
       [
         { char: "a", value: "a" },
@@ -158,20 +111,116 @@ export const VirtualKeyboard = () => {
         { char: ".", value: "." },
         { char: "/", value: "/" },
         {
-          char: "Shift",
+          char: keyboardState.permanentShift ? (
+            <ArrowBigUpDashIcon className="size-[18px] stroke-[1.5]" />
+          ) : (
+            <ArrowBigUpIcon className="size-[18px] stroke-[1.5]" />
+          ),
           value: "",
           minWidth: "120px",
-          onClick: () => dispatch({ type: "TOGGLE_TEMP_SHIFT" }),
+          onClick: () => {
+            if (keyboardState.permanentShift) {
+              dispatch({ type: "TOGGLE_PERMANENT_SHIFT" });
+              return;
+            }
+            dispatch({ type: "TOGGLE_TEMP_SHIFT" });
+          },
+          onDblClick: () => dispatch({ type: "TOGGLE_PERMANENT_SHIFT" }),
         },
       ],
       [
+        {
+          char: <GlobeIcon className="size-[18px] stroke-[1.5]" />,
+          value: "",
+          onClick: () => dispatch({ type: "SWITCH_LANGUAGE", language: Language.Number }),
+        },
         { char: ".com", value: ".com" },
         { char: "@", value: "@" },
         { char: "Space", value: " ", minWidth: "80%" },
-        { char: "X", value: "", onClick: () => virtualKeyboard.clearFocus() },
+        {
+          char: <ChevronDownIcon className="size-[18px] stroke-[1.5]" />,
+          value: "",
+          onClick: () => virtualKeyboard.clearFocus(),
+        },
       ],
     ],
-    lowernumber: [
+    [KeyboardVariant.Uppercase]: [
+      [
+        { char: "Q", value: "Q" },
+        { char: "W", value: "W" },
+        { char: "E", value: "E" },
+        { char: "R", value: "R" },
+        { char: "T", value: "T" },
+        { char: "Y", value: "Y" },
+        { char: "U", value: "U" },
+        { char: "I", value: "I" },
+        { char: "O", value: "O" },
+        { char: "P", value: "P" },
+        { char: "[", value: "[" },
+        { char: "]", value: "]" },
+        { char: "\\", value: "\\" },
+        { char: <DeleteIcon className="size-[18px] stroke-[1.5]" />, value: "", minWidth: "140px" },
+      ],
+      [
+        { char: "A", value: "A" },
+        { char: "S", value: "S" },
+        { char: "D", value: "D" },
+        { char: "F", value: "F" },
+        { char: "G", value: "G" },
+        { char: "H", value: "H" },
+        { char: "J", value: "J" },
+        { char: "K", value: "K" },
+        { char: "L", value: "L" },
+        { char: ";", value: ";" },
+        { char: "'", value: "'" },
+        { char: "Enter", value: "\n", minWidth: "160px" },
+      ],
+      [
+        { char: "Z", value: "Z" },
+        { char: "X", value: "X" },
+        { char: "C", value: "C" },
+        { char: "V", value: "V" },
+        { char: "B", value: "B" },
+        { char: "N", value: "N" },
+        { char: "M", value: "M" },
+        { char: ",", value: "," },
+        { char: ".", value: "." },
+        { char: "/", value: "/" },
+        {
+          char: keyboardState.permanentShift ? (
+            <ArrowBigUpDashIcon className="size-[18px] stroke-[1.5]" />
+          ) : (
+            <ArrowBigUpIcon className="size-[18px] stroke-[1.5]" />
+          ),
+          value: "",
+          minWidth: "120px",
+          onClick: () => {
+            if (keyboardState.permanentShift) {
+              dispatch({ type: "TOGGLE_PERMANENT_SHIFT" });
+              return;
+            }
+            dispatch({ type: "TOGGLE_TEMP_SHIFT" });
+          },
+          onDblClick: () => dispatch({ type: "TOGGLE_PERMANENT_SHIFT" }),
+        },
+      ],
+      [
+        {
+          char: <GlobeIcon className="size-[18px] stroke-[1.5]" />,
+          value: "",
+          onClick: () => dispatch({ type: "SWITCH_LANGUAGE", language: Language.Number }),
+        },
+        { char: ".com", value: ".com" },
+        { char: "@", value: "@" },
+        { char: "Space", value: " ", minWidth: "80%" },
+        {
+          char: <ChevronDownIcon className="size-[18px] stroke-[1.5]" />,
+          value: "",
+          onClick: () => virtualKeyboard.clearFocus(),
+        },
+      ],
+    ],
+    [KeyboardVariant.Number]: [
       [
         { char: "1", value: "1" },
         { char: "2", value: "2" },
@@ -188,13 +237,31 @@ export const VirtualKeyboard = () => {
         { char: "9", value: "9" },
       ],
       [
-        { char: "Shift", value: "", onClick: () => dispatch({ type: "TOGGLE_TEMP_SHIFT" }) },
+        {
+          char: "Shift",
+          value: "",
+          onClick: () => {
+            if (keyboardState.permanentShift) {
+              dispatch({ type: "TOGGLE_PERMANENT_SHIFT" });
+              return;
+            }
+            dispatch({ type: "TOGGLE_TEMP_SHIFT" });
+          },
+          onDblClick: () => dispatch({ type: "TOGGLE_PERMANENT_SHIFT" }),
+        },
         { char: "0", value: "0" },
         { char: "-", value: "-" },
       ],
-      [{ char: "Backspace", value: "" }],
+      [
+        {
+          char: <GlobeIcon className="size-[18px] stroke-[1.5]" />,
+          value: "",
+          onClick: () => dispatch({ type: "SWITCH_LANGUAGE", language: Language.English }),
+        },
+        { char: <DeleteIcon className="size-[18px] stroke-[1.5]" />, value: "", minWidth: "140px" },
+      ],
     ],
-    uppernumber: [
+    [KeyboardVariant.ShiftNumber]: [
       [
         { char: "!", value: "!" },
         { char: "/", value: "/" },
@@ -211,27 +278,45 @@ export const VirtualKeyboard = () => {
         { char: "(", value: "(" },
       ],
       [
-        { char: "Shift", value: "", onClick: () => dispatch({ type: "TOGGLE_TEMP_SHIFT" }) },
+        {
+          char: "Shift",
+          value: "",
+          onClick: () => {
+            if (keyboardState.permanentShift) {
+              dispatch({ type: "TOGGLE_PERMANENT_SHIFT" });
+              return;
+            }
+            dispatch({ type: "TOGGLE_TEMP_SHIFT" });
+          },
+          onDblClick: () => dispatch({ type: "TOGGLE_PERMANENT_SHIFT" }),
+        },
         { char: ")", value: ")" },
         { char: "+", value: "+" },
       ],
-      [{ char: "Backspace", value: "" }],
+      [
+        {
+          char: <GlobeIcon className="size-[18px] stroke-[1.5]" />,
+          value: "",
+          onClick: () => dispatch({ type: "SWITCH_LANGUAGE", language: Language.English }),
+        },
+        { char: <DeleteIcon className="size-[18px] stroke-[1.5]" />, value: "", minWidth: "140px" },
+      ],
     ],
   };
 
   // 計算目前使用哪個 layout
-  const layoutKey = (() => {
-    switch (keyboardState.mode) {
-      case "english": {
-        if (isShiftActive(keyboardState)) return "uppercase";
-        return "lowercase";
+  const layoutKey: KeyboardVariant = (() => {
+    switch (keyboardState.language) {
+      case Language.English: {
+        if (isShiftActive(keyboardState)) return KeyboardVariant.Uppercase;
+        return KeyboardVariant.Lowercase;
       }
-      case "number": {
-        if (isShiftActive(keyboardState)) return "uppernumber";
-        return "lowernumber";
+      case Language.Number: {
+        if (isShiftActive(keyboardState)) return KeyboardVariant.ShiftNumber;
+        return KeyboardVariant.Number;
       }
       default:
-        return "lowercase";
+        return KeyboardVariant.Lowercase;
     }
   })();
 
@@ -241,20 +326,11 @@ export const VirtualKeyboard = () => {
       className="z-50 mx-auto flex w-fit items-center gap-2 rounded bg-gray-200 px-2 py-1 text-black"
     >
       <div className="flex flex-col gap-2">
-        <div className="mb-1 flex flex-row justify-end">
-          <button
-            type="button"
-            className="mr-2 rounded bg-blue-200 px-2 py-1 font-bold text-xs hover:bg-blue-400"
-            onClick={handleSwitchLayout}
-          >
-            {keyboardState.permanentShift ? "切換小寫" : "切換大寫"}
-          </button>
-        </div>
         {keyboardLayouts[layoutKey].map((row, rowIndex) => (
-          <div key={rowIndex} className="flex flex-row flex-nowrap gap-2">
+          <div key={layoutKey + rowIndex} className="flex flex-row flex-nowrap gap-2">
             {row.map((key) => (
               <KeyButton
-                key={key.char}
+                key={key.char?.toString()}
                 label={key.char}
                 minWidth={key.minWidth}
                 onClick={async () => {
@@ -262,10 +338,10 @@ export const VirtualKeyboard = () => {
                     key.onClick();
                   } else {
                     await virtualKeyboard.typeCharacter(key.value);
-                    // 輸入完單字後重置暫時 Shift
                     dispatch({ type: "RESET_TEMP_SHIFT" });
                   }
                 }}
+                onDblClick={key.onDblClick}
               />
             ))}
           </div>
@@ -275,13 +351,24 @@ export const VirtualKeyboard = () => {
   );
 };
 
-const KeyButton = ({ label, minWidth, onClick }: { label: string; minWidth?: string; onClick?: () => void }) => {
+const KeyButton = ({
+  label,
+  minWidth,
+  onClick,
+  onDblClick,
+}: {
+  label: ReactNode;
+  minWidth?: string;
+  onClick?: () => void;
+  onDblClick?: () => void;
+}) => {
   return (
     <button
       type="button"
       style={{ minWidth }}
-      className="key size-12 flex-1 rounded-md bg-gray-50 text-lg hover:bg-gray-300 focus:outline-none sm:size-14 lg:size-16"
+      className="key bounded-md flex size-12 w-full flex-1 items-center justify-center bg-gray-50 text-lg hover:bg-gray-300 focus:outline-none sm:size-14 lg:size-16"
       onClick={onClick}
+      onDoubleClick={onDblClick}
     >
       {label}
     </button>
